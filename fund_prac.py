a670e442a9f08b6604e72808b760f14a62aadc7c
MarcelinoV <marcelinomv98x@gmail.com>
2021-06-16 12:39:18 -0400

    declared new variables

--- fund_prac.py
+++ fund_prac.py
@@ -1,1 +1,21 @@
-print("Hello World!!!")
+#print("Hello World!!!")
+
+# prints hello world
+
+# STEP 1
+name = "LeBron" # declaring name variable
+age = 35 # declared age var
+salary = 37.44 # declared salary var
+plays_basketball = True # declared boolean var
+jersey_number = "23" # declared jersey number
+
+# STEP 2
+car_name = "Ford"
+x = 50
+my_first_name = "John"
+
+print(type(name))
+print(type(age))
+print(type(salary))
+print(type(plays_basketball))
+print(type(jersey_number))
